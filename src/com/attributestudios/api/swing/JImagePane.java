874fb2b13f09df96c1cf38c89efe231c5662c43c
Bridger Maskrey <maskreybe@live.com>
2014-10-22 14:16:47 -0400

    JImagePane draw updates Enabled antialiasing and added text alignment

--- src/com/attributestudios/api/swing/JImagePane.java
+++ src/com/attributestudios/api/swing/JImagePane.java
@@ -1,11 +1,14 @@
 package com.attributestudios.api.swing;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
+import javax.swing.SwingConstants;
 
 public class JImagePane extends JComponent
 {
@@ -14,24 +17,30 @@ public class JImagePane extends JComponent
 	
 	private Point2D imageSize;
 	
-	protected BufferedImage theImage;
+	protected BufferedImage image;
 	private String	text;
+	
+	private int horizontalAlignment;
+	private int verticalAlignment;
 	
 	public JImagePane(BufferedImage image)
 	{
 		super();
 		this.setImage(image);
+		
+		this.verticalAlignment = SwingConstants.BOTTOM;
+		this.horizontalAlignment = SwingConstants.CENTER;
 	}
 	
 	public void setImage(BufferedImage image)
 	{
 		synchronized(this.lock)
 		{
-			this.theImage = image;
+			this.image = image;
 			
-			if(this.theImage != null)
+			if(this.image != null)
 			{
-				this.imageSize = new Point(this.theImage.getWidth(), this.theImage.getHeight());
+				this.imageSize = new Point(this.image.getWidth(), this.image.getHeight());
 			} else
 			{
 				this.imageSize = new Point(1, 1);
@@ -41,11 +50,40 @@ public class JImagePane extends JComponent
 		this.repaint();
 	}
 	
+	public BufferedImage getImage()
+	{
+		return this.image;
+	}
+	
+	public void setVerticalAlignment(int alignment)
+	{
+		this.verticalAlignment = alignment;
+	}
+	
+	public int getVerticalAlignment()
+	{
+		return this.verticalAlignment;
+	}
+	
+	public void setHorizontalAlignment(int alignment)
+	{
+		this.horizontalAlignment = alignment;
+	}
+	
+	public int getHorizontalAlignment()
+	{
+		return this.horizontalAlignment;
+	}
+	
 	@Override
 	public synchronized void paint(Graphics g)
 	{		
-		g.setColor(this.getBackground());
-		g.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		g2d.setColor(this.getBackground());
+		g2d.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
 		
 		float xRatio = (float) (this.getWidth() / this.imageSize.getX());
 		float yRatio = (float) (this.getHeight() / this.imageSize.getY());
@@ -66,7 +104,7 @@ public class JImagePane extends JComponent
 		int diffSizeX = (int)((this.getWidth() - newWidth) * 0.5F);
 		int diffSizeY = (int)((this.getHeight() - newHeight) * 0.5F);
 		
-		g.drawImage(this.theImage, diffSizeX, diffSizeY, newWidth, newHeight, this);
+		g2d.drawImage(this.image, diffSizeX, diffSizeY, newWidth, newHeight, this);
 		
 		if(this.getBorder() != null)
 		{
@@ -75,8 +113,48 @@ public class JImagePane extends JComponent
 		
 		if(this.getText() != null)
 		{
-			g.setColor(this.getForeground());
-			g.drawString(this.getText(), this.getWidth() / 2 - (g.getFontMetrics().stringWidth(this.getText()) / 2), this.getHeight() / 2 - (g.getFontMetrics().getHeight() / 2));
+			g2d.setColor(this.getForeground());
+			
+			int textX;
+			int textY;
+			
+			switch(this.horizontalAlignment)
+			{
+			case SwingConstants.CENTER:
+				textX = this.getWidth() / 2 - (g.getFontMetrics().stringWidth(this.getText()) / 2);
+				break;
+				
+			case SwingConstants.TRAILING:
+			case SwingConstants.RIGHT:
+				textX = this.getWidth() - g.getFontMetrics().stringWidth(this.getText());
+				break;
+				
+			case SwingConstants.LEADING:
+			case SwingConstants.LEFT:
+			default:
+				textX = 0;
+				break;
+			}
+			
+			switch(this.verticalAlignment)
+			{
+			case SwingConstants.BOTTOM:
+				textY = this.getHeight() - g.getFontMetrics().getHeight() - g.getFontMetrics().getLeading();
+				break;
+			
+			case SwingConstants.CENTER:
+				textY = this.getHeight() / 2 - (g.getFontMetrics().getHeight() / 2);
+				break;
+			
+			case SwingConstants.TOP:
+			default:
+				textY = g.getFontMetrics().getLeading();
+				break;
+			}
+			
+			g2d.drawString(this.getText(), textX, textY);
+			
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
 		}
 	}
 	
