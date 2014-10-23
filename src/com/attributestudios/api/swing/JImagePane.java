66104b193633bef583abf2500ac7245163473f0a
Bridger Maskrey <maskreybe@live.com>
2014-10-23 10:36:12 -0400

    JImagePane improvements Added options for caption background and toggle for image aspect ratio preservation.

--- src/com/attributestudios/api/swing/JImagePane.java
+++ src/com/attributestudios/api/swing/JImagePane.java
@@ -1,5 +1,6 @@
 package com.attributestudios.api.swing;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -10,6 +11,10 @@ import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 import javax.swing.SwingConstants;
 
+/**
+ * @author Bridger Maskrey (maskreybe@live.com)
+ * @version 2014-10-23
+ */
 public class JImagePane extends JComponent
 {
 	private static final long	serialVersionUID	= 5437939503106455141L;
@@ -23,13 +28,24 @@ public class JImagePane extends JComponent
 	private int horizontalAlignment;
 	private int verticalAlignment;
 	
+	private boolean textBackground;
+	private Color textBackgroundColor;
+	
+	private boolean preserveAspectRatio;
+	
 	public JImagePane(BufferedImage image)
 	{
 		super();
 		this.setImage(image);
 		
-		this.verticalAlignment = SwingConstants.BOTTOM;
-		this.horizontalAlignment = SwingConstants.CENTER;
+		this.setVerticalAlignment(SwingConstants.CENTER);
+		this.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		this.setTextBackgroundColor(Color.BLACK);
+		
+		this.setPreserveAspectRatio(true);
+		
+		this.setTextBackgroundColor(this.getBackground());
 	}
 	
 	public void setImage(BufferedImage image)
@@ -53,6 +69,16 @@ public class JImagePane extends JComponent
 	public BufferedImage getImage()
 	{
 		return this.image;
+	}
+	
+	public void setTextBackground(boolean textBackground)
+	{
+		this.textBackground = textBackground;
+	}
+	
+	public boolean getTextBackground()
+	{
+		return this.textBackground;
 	}
 	
 	public void setVerticalAlignment(int alignment)
@@ -88,21 +114,33 @@ public class JImagePane extends JComponent
 		float xRatio = (float) (this.getWidth() / this.imageSize.getX());
 		float yRatio = (float) (this.getHeight() / this.imageSize.getY());
 		
-		int newWidth = 0, newHeight = 0;
+		int newWidth, newHeight, diffSizeX, diffSizeY;
 		
-		if(xRatio > yRatio)
+		g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+		
+		if(this.preserveAspectRatio)
 		{
-			newWidth = (int)(this.imageSize.getX() * yRatio);
-			newHeight = (int)(this.imageSize.getY() * yRatio);
+			if(xRatio > yRatio)
+			{
+				newWidth = (int)(this.imageSize.getX() * yRatio);
+				newHeight = (int)(this.imageSize.getY() * yRatio);
+			}
+			else
+			{
+				newWidth = (int)(this.imageSize.getX() * xRatio);
+				newHeight = (int)(this.imageSize.getY() * xRatio);
+			}
+			
+			diffSizeX = (int)((this.getWidth() - newWidth) * 0.5F);
+			diffSizeY = (int)((this.getHeight() - newHeight) * 0.5F);
 		}
 		else
 		{
-			newWidth = (int)(this.imageSize.getX() * xRatio);
-			newHeight = (int)(this.imageSize.getY() * xRatio);
+			newWidth = this.getWidth();
+			newHeight = this.getHeight();
+			
+			diffSizeX = diffSizeY = 0;
 		}
-		
-		int diffSizeX = (int)((this.getWidth() - newWidth) * 0.5F);
-		int diffSizeY = (int)((this.getHeight() - newHeight) * 0.5F);
 		
 		g2d.drawImage(this.image, diffSizeX, diffSizeY, newWidth, newHeight, this);
 		
@@ -111,46 +149,56 @@ public class JImagePane extends JComponent
 			this.getBorder().paintBorder(this, g, this.getX(), this.getY(), this.getWidth(), this.getHeight());
 		}
 		
-		if(this.getText() != null)
+		if(this.getText() != null && !this.getText().isEmpty())
 		{
-			g2d.setColor(this.getForeground());
-			
 			int textX;
 			int textY;
 			
 			switch(this.horizontalAlignment)
 			{
 			case SwingConstants.CENTER:
-				textX = this.getWidth() / 2 - (g.getFontMetrics().stringWidth(this.getText()) / 2);
+				textX = newWidth / 2 - (g2d.getFontMetrics().stringWidth(this.getText()) / 2) + diffSizeX;
 				break;
 				
 			case SwingConstants.TRAILING:
 			case SwingConstants.RIGHT:
-				textX = this.getWidth() - g.getFontMetrics().stringWidth(this.getText());
+				textX = newWidth - g2d.getFontMetrics().stringWidth(this.getText()) + diffSizeX;
 				break;
 				
 			case SwingConstants.LEADING:
 			case SwingConstants.LEFT:
 			default:
-				textX = 0;
+				textX = diffSizeX;
 				break;
 			}
 			
 			switch(this.verticalAlignment)
 			{
 			case SwingConstants.BOTTOM:
-				textY = this.getHeight() - g.getFontMetrics().getHeight() - g.getFontMetrics().getLeading();
+				textY = newHeight - g2d.getFontMetrics().getHeight() - g2d.getFontMetrics().getLeading() + diffSizeY;
 				break;
 			
 			case SwingConstants.CENTER:
-				textY = this.getHeight() / 2 - (g.getFontMetrics().getHeight() / 2);
+				textY = newHeight / 2 - (g2d.getFontMetrics().getHeight() / 2) + diffSizeY;
 				break;
 			
 			case SwingConstants.TOP:
 			default:
-				textY = g.getFontMetrics().getLeading();
+				textY = g.getFontMetrics().getLeading() + diffSizeY;
 				break;
 			}
+			
+			if(this.textBackground)
+			{
+				g2d.setColor(this.textBackgroundColor);
+				
+				g2d.fillRect(textX - (g2d.getFontMetrics().getMaxAdvance() / 4),
+							 textY - g2d.getFontMetrics().getHeight() + g2d.getFontMetrics().getMaxDescent(), 
+							 g2d.getFontMetrics().stringWidth(this.getText()) + (g2d.getFontMetrics().getMaxAdvance() / 2),
+							 g2d.getFontMetrics().getHeight() + g2d.getFontMetrics().getLeading());
+			}
+			
+			g2d.setColor(this.getForeground());
 			
 			g2d.drawString(this.getText(), textX, textY);
 			
@@ -166,5 +214,25 @@ public class JImagePane extends JComponent
 	public void setText(String text)
 	{
 		this.text = text;
+	}
+
+	public Color getTextBackgroundColor()
+	{
+		return this.textBackgroundColor;
+	}
+
+	public void setTextBackgroundColor(Color textBackgroundColor)
+	{
+		this.textBackgroundColor = textBackgroundColor;
+	}
+
+	public boolean getPreserveAspectRatio()
+	{
+		return this.preserveAspectRatio;
+	}
+
+	public void setPreserveAspectRatio(boolean preserveAspectRatio)
+	{
+		this.preserveAspectRatio = preserveAspectRatio;
 	}
 }
